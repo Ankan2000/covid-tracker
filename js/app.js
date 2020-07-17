df7e76634f1f9290b46e613a75f16409ea1a5fcb
Ankan2000 <kakolibag44@gmail.com>
2020-07-17 21:52:20 +0530

    card animation and updation added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
+import { CountUp } from "./countUp.min.js";
+
 const select = document.querySelector("#countrySelector");
+const confirmedNumberCard = document.querySelector("#confirmed");
+const recoveryNumberCard = document.querySelector("#recovery");
+const deathsNumberCard = document.querySelector("#deaths");
 const chartBlock = document.querySelector('#visualChart').getContext('2d');
 
 const today = new Date();
@@ -30,8 +35,18 @@ const setOptions = async () => {
 
 }
 
+const updateCards = (confirmed, deaths, recovered) => {
+    let countUp = new CountUp(confirmedNumberCard, confirmed);
+    countUp.start();
+
+    countUp = new CountUp(deathsNumberCard, deaths);
+    countUp.start();
+
+    countUp = new CountUp(recoveryNumberCard, recovered);
+    countUp.start();
+}
+
 const updateChart = (infections, deaths, recovered, date) => {
-    console.log(deaths[deaths.length - 1]);
     const chart = new Chart(chartBlock, {
         type: 'line',
         data: {
@@ -68,7 +83,7 @@ const updateChart = (infections, deaths, recovered, date) => {
 
 }
 
-fetchTimelineData = async (event) => {
+const fetchTimelineData = async (event) => {
     try {
         const response = await fetch(`${api}/v3/analytics/trend/country?countryCode=${event.target.value}&startDate=${startDate}&endDate=${endDate}`)
         const data = await response.json();
@@ -78,8 +93,12 @@ fetchTimelineData = async (event) => {
         const date = data.map(item => new Date(item.last_updated).toISOString().split('T')[0])
 
         // console.log(deathData);
+        const totalConfirmed = infectionsData[infectionsData.length - 1];
+        const totalDeaths = deathData[deathData.length - 1];
+        const totalRecovered = recoveredData[recoveredData.length - 1];
 
         updateChart(infectionsData, deathData, recoveredData, date);
+        updateCards(totalConfirmed, totalDeaths, totalRecovered);
     } catch (err) {
         console.error(err);
     }
