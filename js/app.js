72444b98bc00a10d4d2bd8e90394b267d51aa298
Ankan2000 <kakolibag44@gmail.com>
2020-07-18 19:04:38 +0530

    i dont know everything is broking right now

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
+import { CountUp } from "./countUp.min.js";
+import { createChart } from "./charts";
+
 const select = document.querySelector("#countrySelector");
+const confirmedNumberCard = document.querySelector("#confirmed");
+const recoveryNumberCard = document.querySelector("#recovery");
+const deathsNumberCard = document.querySelector("#deaths");
 const chartBlock = document.querySelector('#visualChart').getContext('2d');
 
 const today = new Date();
@@ -7,6 +13,8 @@ const startDate = today.getFullYear() + "-0" + (currentMonth - 6) + "-" + today.
 const endDate = today.getFullYear() + "-" + currentMonth + "-" + today.getDate();
 
 const api = "https://api.coronatracker.com";
+
+let chart;
 
 const fetchData = async () => {
     try {
@@ -28,47 +36,23 @@ const setOptions = async () => {
         select.add(options);
     })
 
+    chart = createChart();
 }
 
-const updateChart = (infections, deaths, recovered, date) => {
-    console.log(deaths[deaths.length - 1]);
-    const chart = new Chart(chartBlock, {
-        type: 'line',
-        data: {
-            labels: date,
-            datasets: [
-                {
-                    label: "Confirmed",
-                    data: infections,
-                    backgroundColor: '#cce8ff',
-                    borderColor: '#008cff',
-                    pointRadius: 0,
-                },
-                {
-                    label: "Recovered",
-                    data: recovered,
-                    backgroundColor: '#dfffd6',
-                    borderColor: '#25aa00',
-                    pointRadius: 0,
-                },
-                {
-                    label: "Deaths",
-                    data: deaths,
-                    backgroundColor: '#fdc5c5',
-                    borderColor: '#ff0000',
-                    pointRadius: 0,
-                }
-            ]
-        },
-        options: {
-            response: true,
-            maintainAspectRatio: false
-        }
-    })
+const updateCards = (confirmed, deaths, recovered) => {
+    let countUp = new CountUp(confirmedNumberCard, confirmed);
+    countUp.start();
 
+    countUp = new CountUp(deathsNumberCard, deaths);
+    countUp.start();
+
+    countUp = new CountUp(recoveryNumberCard, recovered);
+    countUp.start();
 }
 
-fetchTimelineData = async (event) => {
+
+
+const fetchTimelineData = async (event) => {
     try {
         const response = await fetch(`${api}/v3/analytics/trend/country?countryCode=${event.target.value}&startDate=${startDate}&endDate=${endDate}`)
         const data = await response.json();
@@ -78,8 +62,12 @@ fetchTimelineData = async (event) => {
         const date = data.map(item => new Date(item.last_updated).toISOString().split('T')[0])
 
         // console.log(deathData);
+        const totalConfirmed = infectionsData[infectionsData.length - 1];
+        const totalDeaths = deathData[deathData.length - 1];
+        const totalRecovered = recoveredData[recoveredData.length - 1];
 
         updateChart(infectionsData, deathData, recoveredData, date);
+        updateCards(totalConfirmed, totalDeaths, totalRecovered);
     } catch (err) {
         console.error(err);
     }
