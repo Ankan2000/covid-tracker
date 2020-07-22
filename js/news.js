acd5de2aa45141834c106890e7d1288524c2b971
Ankan2000 <kakolibag44@gmail.com>
2020-07-22 19:17:18 +0530

    corrected the news timing data

--- js/news.js
+++ js/news.js
@@ -1,9 +1,5 @@
 const newsSection = document.querySelector(".news__body");
 
-// const icon = document.createElement("i");
-// icon.classList.add("fa");
-// icon.classList.add("fa-history");
-
 export const createNews = (
   newsUrl,
   newsImage,
@@ -20,26 +16,33 @@ export const createNews = (
 
   const image = document.createElement("img");
   image.setAttribute("src", newsImage);
-  image.setAttribute("width", "80px");
-  image.setAttribute("height", "80px");
+  image.setAttribute("width", "100px");
+  image.setAttribute("height", "100px");
   image.classList.add("news__image");
 
   const newsDiv = document.createElement("div");
   newsDiv.classList.add("news__details");
 
   const title = document.createElement("h2");
-  title.innerText = newsTitle;
+  title.innerText = newsTitle.slice(0, 30) + "...";
   title.classList.add("news__title");
 
   const description = document.createElement("p");
-  description.innerText = newsDescription;
+  description.innerText = newsDescription.slice(0, 100) + "....";
   description.classList.add("news__description");
 
   const time = document.createElement("small");
   time.classList.add("news__time");
 
-  // time.appendChild(icon);
-  time.innerText = getTimeDifference(newsTime);
+  const icon = document.createElement("i");
+  icon.classList.add("fa");
+  icon.classList.add("fa-history");
+  time.appendChild(icon);
+  // time.innerText = getTimeDifference(newsTime);
+  const timeText = document.createTextNode(
+    getTimeDifference(newsTime) + " ago"
+  );
+  time.appendChild(timeText);
 
   newsDiv.appendChild(title);
   newsDiv.appendChild(description);
@@ -55,8 +58,19 @@ export const createNews = (
 };
 
 function getTimeDifference(time) {
-  let currentTime = new Date().getMinutes();
   time = new Date(time);
-  let givenTime = time.getMinutes();
-  return currentTime - givenTime;
+  let timeMinutes = time.getMinutes();
+  let timeHours = time.getHours() + 1;
+
+  let currentTime = new Date();
+  let currentMinutes = currentTime.getMinutes();
+  let currentHours = currentTime.getHours() + 1;
+
+  let hourDifference = currentHours - timeHours;
+  let minutesDifference =
+    currentMinutes < timeMinutes
+      ? timeMinutes - currentMinutes
+      : currentMinutes - timeMinutes;
+
+  return `${hourDifference}hrs and ${minutesDifference}mins`;
 }
